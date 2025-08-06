7d8fabb3239b47df94ed484e7afad3f1e1bd8db4
badmurphiee <badmusmarruf@gmail.com>
2025-08-06 19:59:41 -0400

    update changes

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,8 +4,8 @@ window.PETSTAY_CONFIG = {
   COGNITO_USER_POOL_CLIENT_ID: "rdcatpv4ggdd244d30sv0vs75",
   COGNITO_DOMAIN: "us-east-2fj0stvuy7.auth.us-east-2.amazoncognito.com",
   REDIRECT_SIGN_IN_URL: "https://main.d3ucnervso97zx.amplifyapp.com/admin-frontend/post-login.html",
-  REDIRECT_SIGN_OUT_URL: "https://https://main.d3ucnervso97zx.amplifyapp.com/index.html",
-  REDIRECT_ADMIN_SIGN_IN_URL: "https://main.d3ucnervso97zx.amplifyapp.com/admin-frontend/post-login.html"
+  REDIRECT_SIGN_OUT_URL:"https://main.d3ucnervso97zx.amplifyapp.com/index.html",
+  REDIRECT_ADMIN_SIGN_IN_URL:"https://main.d3ucnervso97zx.amplifyapp.com/admin-frontend/post-login.html"
   BOOKINGS_API_URL: "{{BOOKINGS_API_URL}}",
   ROOMS_AVAILABILITY_API_URL: "{{ROOMS_AVAILABILITY_API_URL}}",
   NEW_BOOKING_API_URL: "{{NEW_BOOKING_API_URL}}",
